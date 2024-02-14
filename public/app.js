eda4bc7b767958131a09a07feb49a999863522e0
allanMartin <z54360907@gmail.com>
2024-02-14 13:20:40 +0700

    feature: click to see image

--- public/app.js
+++ public/app.js
@@ -6,7 +6,7 @@ import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitC
 var renderer, scene, camera, heart, controls, cubes = [], groups = [];
 var geometry = new THREE.BoxGeometry(60, 60, 60);
 var offset = 70;
-var animation = new TimelineMax({ repeat: -1, delay: 1 });
+var animation = new TimelineMax({ repeat: -1, delay: 2 });
 
 function initScene() {
     scene = new THREE.Scene();
@@ -26,7 +26,7 @@ function initRenderer() {
     renderer.setClearColor(0xF6F1E9);
     document.body.appendChild(renderer.domElement);
 
-    controls = new OrbitControls(camera, renderer.domElement);
+    // controls = new OrbitControls(camera, renderer.domElement);
 }
 
 
@@ -87,16 +87,13 @@ function createLevel(level, lineCount, excludeCenter) {
             group.add( outlineMesh2 );
             cubes.push(outlineMesh2);
         } else {
-
-        
-
-        var outlineMaterial2 = new THREE.MeshBasicMaterial( { color: 0xFF55BB, side: THREE.BackSide } );
-        var outlineMesh2 = new THREE.Mesh( geometry, outlineMaterial2 );
-        outlineMesh2.position.set(x, 0, offset / 2);
-        // outlineMesh2.position.set(x, 0, -offset / 2);
-        outlineMesh2.scale.multiplyScalar(1.05);
-	    group.add( outlineMesh2 );
-        cubes.push(outlineMesh2);
+            var outlineMaterial2 = new THREE.MeshBasicMaterial( { color: 0xFF55BB, side: THREE.BackSide } );
+            var outlineMesh2 = new THREE.Mesh( geometry, outlineMaterial2 );
+            outlineMesh2.position.set(x, 0, offset / 2);
+            // outlineMesh2.position.set(x, 0, -offset / 2);
+            outlineMesh2.scale.multiplyScalar(1.05);
+            group.add( outlineMesh2 );
+            cubes.push(outlineMesh2);
         }
 
         group.add(cube0);
@@ -123,7 +120,7 @@ function fitToHeart() {
             cube.position.y = 200;
             cube.position.z = 700;            
         }
-        animation.to(cube.position, 2, { x: x, y: y, z: z, ease: Expo.easeInOut }, 0);
+        animation.to(cube.position, 5, { x: x, y: y, z: z, ease: Expo.easeInOut }, 0);
     }
 }
 
@@ -137,9 +134,65 @@ function rotateHeart() {
         var group = groups[i];
         animation
             .to(group.rotation, 0.4, { y: -0.2 }, 0)
-            .to(group.rotation, 2, { y: 2 * Math.PI, ease: Expo.easeInOut }, 1.4 - 0.2 * group.level);
+            .to(group.rotation, 5, { y: 2 * Math.PI, ease: Expo.easeInOut }, 1.4 - 0.2 * group.level);
     }
 }
+
+const raycaster = new THREE.Raycaster();
+const mousePos = new THREE.Vector2();
+
+function intersect(mousePos) {
+    raycaster.setFromCamera(mousePos, camera);
+    return raycaster.intersectObjects(scene.children);
+}
+
+window.addEventListener("click", (event) => {
+
+    mousePos.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mousePos.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    var animation = new TimelineMax();
+    var cube_clone = null;
+    const found = intersect(mousePos);
+
+    if (found.length > 0) {
+        const cube_found = found[0].object;
+        cube_clone = cube_found.clone();
+        cube_clone.name = "clone";
+        cube_clone.position.set(0, 0, 0);
+
+        var outlineMaterial2 = new THREE.MeshBasicMaterial( { color: 0xFF55BB, side: THREE.BackSide } );
+        var outlineMesh2 = new THREE.Mesh( geometry, outlineMaterial2 );
+        outlineMesh2.name = "outline_clone";
+        outlineMesh2.position.set(0, 0, 0);
+        outlineMesh2.scale.multiplyScalar(1.05);
+
+        scene.add(outlineMesh2);
+        scene.add(cube_clone);
+
+        animation.to(cube_clone.position, 2, { x: 1500, y: 200, z: 500, ease: Expo.easeInOut }, 0);
+        animation.to(cube_clone.rotation, 2, { y: Math.PI * 1.2, ease: Expo.easeInOut}, 0);
+        animation.to(cube_clone.scale, 2, {x: 1.5, y: 1.5, z: 1.5, ease: Expo.easeInOut}, 0);
+
+        animation.to(outlineMesh2.position, 2, { x: 1500, y: 200, z: 500, ease: Expo.easeInOut }, 0);
+        animation.to(outlineMesh2.rotation, 2, { y: Math.PI * 1.2, ease: Expo.easeInOut}, 0);
+        animation.to(outlineMesh2.scale, 2, {x: 1.55, y: 1.55, z: 1.55, ease: Expo.easeInOut}, 0);
+
+        camera.lookAt(cube_clone.position);
+        animation.to(camera.position, 2, { x: 1500, y: 300, z: 800, ease: Expo.easeInOut }, 0); 
+    } else {
+        var cloneObj = scene.getObjectByName("clone");
+        var outlineCloneObj = scene.getObjectByName("outline_clone");
+        animation.to(camera.position, 2, { x: 0, y: 300, z: 800, ease: Expo.easeInOut }, 0);
+        setTimeout(() => {
+            scene.remove(cloneObj);
+            scene.remove(outlineCloneObj);
+        }, 1000);
+    }
+    
+
+    // console.log(found);
+});
 
 initScene();
 initCamera();
